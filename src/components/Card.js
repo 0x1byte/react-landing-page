f805377b8ca95810793293144fdffc585802d78a
0x1byte <mahdikh6@gmail.com>
2022-03-01 13:58:30 +0330

    update price & rendering of card

--- src/components/Card.js
+++ src/components/Card.js
@@ -31,7 +31,7 @@ export class Card extends Component {
           <img src={image} alt=""/>
           <h3>{name}</h3>
           <div className={styles.info}>
-            <p>{price}</p>
+            <p>{price} {counter ? `* ${counter} = ${counter * Number(price.split(" ")[0])} $` : ""}</p>
             <div className={styles.counter}>
               <img className={this.state.counter ? "" : styles.deactive} src={down} alt="down arrow" onClick={this.downHandler}/>
               <span>{counter}</span>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,15 +5,24 @@ import iphonex from "../images/iphonex.jpg";
 import iphone11 from "../images/iphone11.jpg";
 import iphone12 from "../images/iphone12.jpg";
 import iphone13 from "../images/iphone13.jpg";
+import {v4} from "uuid";
 
 export class Cards extends Component {
+  constructor(){
+    super();
+    this.state = {
+      phoneData:[
+        {id:v4(),image:iphonex, name:"Iphone X" ,price:"650 $"},
+        {id:v4(),image:iphone11, name:"Iphone 11" ,price:"750 $"},
+        {id:v4(),image:iphone12, name:"Iphone 12" ,price:"850 $"},
+        {id:v4(),image:iphone13, name:"Iphone 13" ,price:"950 $"}
+      ]
+    }
+  }
   render() {
     return (
       <div className={styles.container}>
-          <Card image={iphonex} name="Iphone X" price="650 $" />
-          <Card image={iphone11} name="Iphone 11" price="750 $" />
-          <Card image={iphone12} name="Iphone 12" price="850 $" />
-          <Card image={iphone13} name="Iphone 13" price="950 $" />
+          {this.state.phoneData.map(phone => <Card key={phone.id} image={phone.image} name={phone.name} price={phone.price} />)}
       </div>
     )
   }
